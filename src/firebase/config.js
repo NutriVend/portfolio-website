73f5a7b7565be813aa008ad6b60a4bdb79e29841
NutriVend <112494172+NutriVend@users.noreply.github.com>
2025-02-12 19:24:59 +0900

    redeacted api keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,13 +9,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyATENARIfBZ57HAhtJGKGIYjeXQs6jOktE",
-  authDomain: "portfolio-website-6affe.firebaseapp.com",
-  projectId: "portfolio-website-6affe",
-  storageBucket: "portfolio-website-6affe.firebasestorage.app",
-  messagingSenderId: "620730873657",
-  appId: "1:620730873657:web:d0c835b9c0cbd083dd1ae8",
-  measurementId: "G-WBKC2V99XS"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
